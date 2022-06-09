39bc2cddda75cfc1cbcec45357b597de3f64a1d4
dina-kvivesen <71272716+dina-kvivesen@users.noreply.github.com>
2022-06-09 10:40:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <Navbar/>
     <div>
     <h1 className='mt-96 text-3xl sm:text-6xl font-bold text-center text-gray-900'>Hi! I&apos;m Dina</h1>
-    <p className='mx-auto text-center' >here are some of my projects created during my front-end studies</p>
+    <p className='mx-auto text-center mt-4' >here are some of my projects created during my front-end studies</p>
     <div className="pt-4 flex flex-wrap gap-2 items-center justify-center">
     <Button href="/projects">
       View Projects
